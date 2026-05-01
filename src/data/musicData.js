4e8e66b35c1e104528ce80e8cfb1cb45b6484de8
abdervalvocalgroup-design <abdervalvocalgroup@gmail.com>
2026-05-01 11:11:31 +0200

    Create musicData.js

---
+++ src/data/musicData.js
@@ -0,0 +1,51 @@
+import album1 from "../assets/album1.jpg";
+import album2 from "../assets/album2.jpg";
+
+export const albums = [
+  {
+    id: "protegidos-de-deus",
+    title: "Protegidos De Deus",
+    year: 2025,
+    cover: album1,
+    tracks: [
+      { title: "Protegidos De Deus", file: "https://drive.google.com/uc?export=download&id=1sr5TzmIK9Zip8qGmkAtznq3WVUKhZZ62" },
+      { title: "Mwari Uyai", file: "https://drive.google.com/uc?export=download&id=1OJ1VgFztu9qMY_2v08gM7qLsH1y9ohdL" },
+      { title: "Jesus Is Coming Again", file: "https://drive.google.com/uc?export=download&id=1sT2ve_DeGOhd3MI-KZtyCRbcNpqm1xRm" },
+      { title: "Muzitaye", file: "https://drive.google.com/uc?export=download&id=1eW5ID4uVZSL5xggv7bcRgYkSHZXvms9X" },
+      { title: "Quando Cristo Regressar", file: "https://drive.google.com/uc?export=download&id=1Qryw0Moa5YIumyKZrTuG0lMbYvnp_65A" },
+      { title: "Os 10 Mandamentos", file: "https://drive.google.com/uc?export=download&id=1QTQFZWaxrdBU_liqqi4JKPi7N2Zaythd" },
+      { title: "Mene, Tequel, Ufarsim", file: "https://drive.google.com/uc?export=download&id=1PeZo89Hn1pXREj15ynz7Q9817_YEHOnS" },
+      { title: "Mudzetu", file: "https://drive.google.com/uc?export=download&id=1Lxrj5cyxkoG_vXpXpTxKmsaizkuVdjOP" },
+      { title: "Mutchado", file: "https://drive.google.com/uc?export=download&id=1u1DjM4OUwBO9Gkm6ysulspJo9SqYTTUk" },
+      { title: "Vanotya Vanozotiza", file: "https://drive.google.com/uc?export=download&id=1Uz9_4LaucAoeR-qy-ibEj1jGfRaj8o_F" },
+      { title: "Ari Nane", file: "https://drive.google.com/uc?export=download&id=1E_57UE6z3ynBMpQm9aWkwswzl_3lqCyd" },
+      { title: "Nzimu Woyera", file: "https://drive.google.com/uc?export=download&id=1VVkPN78nv57_iyYaB0iu3FLZ9dUjHAgr" },
+    ],
+  },
+  {
+    id: "as-origens",
+    title: "As Origens",
+    year: 2024,
+    cover: album2,
+    tracks: [
+      { title: "As Origens", file: "https://drive.google.com/uc?export=download&id=1ow8T4KTmqh2LdlqDOrQF8QnwDxaQAzj9" },
+      { title: "Noah", file: "https://drive.google.com/uc?export=download&id=1U3bREbAMpEO24VSfVeu0smwjBRIwJ-Fj" },
+      { title: "Ndoda Kuvenekwa", file: "https://drive.google.com/uc?export=download&id=1-xGstqQpjE_YsQhiEXKFHLqDmua51lpn" },
+      { title: "Jesus, Wonderful Savior", file: "https://drive.google.com/uc?export=download&id=1u1qsA2JTqd5JUUHisiHN4YSBHl3pjDXJ" },
+      { title: "Mordomo Fiel", file: "https://drive.google.com/uc?export=download&id=1g6iange8zZAPa7qk3JKGwXpGuG-geDj8" },
+      { title: "Nditumei", file: "https://drive.google.com/uc?export=download&id=1TwWFbPAuiM8DQpgxerfUgcQaOCCwrxF2" },
+      { title: "Ele É Redentor", file: "https://drive.google.com/uc?export=download&id=18Dct30UPQBikn2m4MkOA1JzvWdTpVg6O" },
+      { title: "The Three Angel’s Message", file: "https://drive.google.com/uc?export=download&id=1wxDu0GplYCo4vqQ-ew7JLTT2LCOglpAu" },
+      { title: "Hino Da Beira", file: "https://drive.google.com/uc?export=download&id=1QRcmFuHqUu7Wphd_-rohpb1f5t0L8kUU" },
+      { title: "Sinos De Casamento", file: "https://drive.google.com/uc?export=download&id=16t-QUuxpzZnw6r4diuqglZafJTPsyiLN" },
+    ],
+  },
+];
+
+export const streamingLinks = {
+  spotify: "https://open.spotify.com/artist/6CcyJJ8eMc100xugWkY82w",
+  youtube: "https://www.youtube.com/channel/UCkRB40EhAyFWqNjZQx2PycQ",
+  facebook: "https://www.facebook.com/share/v/1Ad6XpfohX/",
+  tiktok: "https://vt.tiktok.com/ZS96x3MER/",
+  instagram: "https://www.instagram.com/abdervalvocalgroup",
+};
